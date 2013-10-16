3e7fc1f61b55a12e81a427302ab366ede63fb2f1
wlm01 (Dr. ) <wolfgang.mauerer@siemens.com>
2013-10-16 13:03:32 +0000

    Re-engineer makeforest.R
    The forest generation mechanism is changed in various ways:
    
    - Multiple additional tm preparation steps are performed to
      create a cleaner version of the base text.
    - Variables use a (hopefully) more systematic and self-explanatory
      naming systematic.
    - Some documentation is added.
    - Hooks for pre- and postprocessing functions that are applied
      on the corpus before and after text mining are provided.
    - Corpus preparation and forest creation are split into to
      functions to indicate the conceptual differences between these
      steps.
    - Explicit loops are replaced with more R-like constructions.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/snatm@76 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/makeforest.r
+++ pkg/R/makeforest.r
@@ -1,30 +1,83 @@
-makeforest <- 
-function (month)
+gen.corpus <- function (ml, repo.path="./", suffix=".txt", outdir=NULL,
+                        marks=character(0), encoding="UTF-8", preprocess=NULL,
+                        postprocess=NULL)
 {
-    tm.plugin.mail::convert_mbox_eml(paste(month, ".txt", sep = ""),
-        paste(month, "/", sep = ""))
-    workingobject <- tm::Corpus(DirSource(month), readerControl = list(reader = readMail(DateFormat = "%a, %d %b %Y %H:%M:%S")))
-    workingobject <- sapply(workingobject, tm.plugin.mail::removeCitation)
-    workingobject <- sapply(workingobject, tm.plugin.mail::removeSignature)
-    threadid <- threads(workingobject)$ThreadID
-    workingobject <- workingobject[!is.na(threadid)]
-    Content <- sapply(sapply(workingobject, "Content"), paste,
-        collapse = "\n")
-    numberofmail <- 1:length(workingobject)
-    authorlist <- sapply(workingobject, "Author")
-    authors <- c()
-    for (i in seq_along(authorlist)) {
-        authors <- c(authors, authorlist[[i]][1])
-    }
-    headinglist <- sapply(workingobject, "Heading")
-    headings <- c()
-    for (i in 1:length(headinglist)) {
-        headings <- c(headings, headinglist[[i]][1])
-    }
-    forest <- cbind(numberofmail, threadid[!is.na(threadid)],
-        authors, headings, Content[!is.na(threadid)])
-    Encoding(forest[,3:5]) <- "latin1"
-    colnames(forest) <- c("emailID", "threadID", "author", "subjects",
-        "content")
-    forest
+  ml.base <- file.path(repo.path, ml)
+  ## Skip the conversion step if the result directory already
+  ## exists. TODO: We need some more intelligent algorithm to deal
+  ## with incremental updates.
+  if (!file.exists(ml.base)) {
+    tm.plugin.mail::convert_mbox_eml(paste(ml.base, suffix, sep = ""),
+                                     paste(ml.base, "/", sep = ""))
+  }
+  corp <- tm::Corpus(DirSource(ml.base, encoding=encoding),
+                     readerControl = list(reader=readMail(DateFormat = "%a, %d %b %Y %H:%M:%S")))
+
+  if (!is.null(preprocess)) {
+    corp <- preprocess(corp)
+  }
+  corp.orig <- corp
+
+  corp <- tm_map(corp, function(x) iconv(enc2utf8(x), sub="byte"))
+  corp <- tm_map(corp, tm.plugin.mail::removeCitation, removeQuoteHeader=T)
+  corp <- tm_map(corp, tm.plugin.mail::removeSignature, marks=marks)
+  corp <- tm_map(corp, tm.plugin.mail::removeMultipart)
+  ## NOTE: It's important to apply tolower before stopword removal;
+  ## otherwise, phrases like "I'm" won't be removed properly
+  corp <- tm_map(corp, tolower)
+  corp <- tm_map(corp, tm::removeWords, stopwords("english"))
+  corp <- tm_map(corp, tm::removeNumbers)
+  corp <- tm_map(corp, tm::removePunctuation)
+  corp <- tm_map(corp, tm::stripWhitespace)
+  corp <- tm_map(corp, tm::stemDocument)
+
+  ## NOTE: stemCompletion should only be done for the terms that are
+  ## actually displayed to a user -- there are only a few ones, and they will
+  ## be very fast to unstem.
+  ##  corp <- tm_map(corp, tm::stemCompletion, dictionary=corp.orig)
+
+  if (!is.null(postprocess)) {
+    corp <- postprocess(corp)
+  }
+
+  return(list(corp=corp, corp.orig=corp.orig))
+}
+
+
+make.forest <- function(corp, encoding="UTF-8") {
+  thread.ids <- threads(corp)$ThreadID
+
+  ## If there are threads without IDs, something is wrong with the
+  ## thread detection. Bail out in this case.
+  if (sum(is.na(thread.ids)))
+    stop("NAs in thread list")
+
+  Content <- sapply(sapply(corp, "Content"), paste,
+                    collapse = "\n")
+
+  ## Provide consecutive identifiers for each message
+  mail.ids <- 1:length(corp)
+
+  ## Extract authors and headings from the corpus and build simple
+  ## character vectors
+  authors <- sapply(corp, function (x) { Author(x)[1] } )
+  attributes(authors) <- NULL
+
+  headings <- sapply(corp, function (x) { Heading(x)[1] } )
+  attributes(headings) <- NULL
+
+  ## The forst collects unique (numeric) identifiers for every mail
+  ## and the thread it is associated with, together with author, subject line
+  ## and content of each mail
+  forest <- cbind(mail.ids, thread.ids, authors,
+                  headings, Content)
+
+  Encoding(forest[,3:5]) <- encoding
+  colnames(forest) <- c("emailID", "threadID", "author", "subject",
+                        "content")
+
+  authors <- do.normalise(forest[,3])
+  forest <- cbind(forest[,1:2], author=authors, forest[,4:5])
+
+  return(forest)
 }
