9cc616473b0bc09571530dc50c6748b86ec48de2
wlm01 (Dr. ) <wolfgang.mauerer@siemens.com>
2013-10-16 13:03:46 +0000

    Refactor centrality.edgelist
    - Remove R-specific default argument
    - Untangle the logic by splitting up deeply nested
      function calls
    - Instead of selecting only networks of a minimum size,
      pick a maximal number of terms that are considered. This
      is beneficial for low-volume mailing lists
    - Use "subject" and "content" for apply.to instead of
      mixing singular and plural forms
    - Return the edgelist and size in case one wants to visualise
      the weight distribution.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/snatm@77 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/centrality.edgelist.r
+++ pkg/R/centrality.edgelist.r
@@ -1,29 +1,52 @@
 library("sna")
 centrality.edgelist <- 
-function (terms, apply.to = c("subjects", "content"), list = c("rhelp", 
-    "rdevel")) 
+function (terms, apply.to, data.path, max.terms=20)
 {
+    if (!(apply.to %in% c("subject", "content")))
+      stop("apply.to must be either 'subject' or 'content'")
     edgelist <- c()
+    keywords <- new.env()
+    sizes <- rep(0, length(terms))
+
     for (i in seq_along(terms)) {
-        if (is.element(unlist(strsplit(file.path(list, apply.to, 
-            paste("net_", terms[i], ".rda", sep = "")), split = "/"))[3], 
-            list.files(paste(list, "/", apply.to, sep = "")))) {
-            load(file.path(list, apply.to, paste("net_", terms[i], 
-                ".rda", sep = "")))
-            if (dim(net)[1] > 19) {
-                net <- sna::component.largest(net, result = "graph", 
-                  connected = "weak")
-                if (dim(net)[1] > 19) {
-                  authors <- rownames(net)
-                  value <- sna::degree(net, cmode = "outdegree")
-                  value <- cbind(authors, value)
-                  value <- ordermatrix(value, 2)
-                  value <- cbind(value[, 1], seq(1:dim(value)[1])/dim(value)[1])
-                  edgelist <- rbind(edgelist, cbind(value[, 1], 
-                    terms[i], value[, 2]))
-                }
-            }
+      tmp <- unlist(strsplit(file.path(data.path, "commnet.terms", apply.to,
+                                       paste("net_", terms[i], ".rda", sep = "")),
+                             split = "/"))
+      tmp <- tmp[length(tmp)]
+      filelist <- list.files(file.path(data.path, "commnet.terms", apply.to))
+
+      if (is.element(tmp, filelist)) {
+        load(file.path(data.path, "commnet.terms", apply.to,
+                       paste("net_", terms[i], ".rda", sep = "")))
+
+        net <- sna::component.largest(net, result = "graph",
+                                      connected = "weak")
+        if (!is.null(dim(net))) {
+          sizes[i] <- dim(net)[1]
         }
+
+        rm(net)
+      }
     }
-    edgelist
+
+    if (max.terms > length(sizes))
+      max.terms <- length(sizes)
+
+    for (i in order(sizes, decreasing=T)[1:max.terms]) {
+      if (sizes[i] == 0)
+        next
+
+      load(file.path(data.path, "commnet.terms", apply.to,
+                     paste("net_", terms[i], ".rda", sep = "")))
+      net <- sna::component.largest(net, result = "graph",
+                                    connected = "weak")
+      authors <- rownames(net)
+      value <- sna::degree(net, cmode = "outdegree")
+      value <- cbind(authors, value)
+      value <- ordermatrix(value, 2)
+      value <- cbind(value[, 1], seq(1:dim(value)[1])/dim(value)[1])
+      edgelist <- rbind(edgelist, cbind(value[, 1], terms[i], value[, 2]))
+    }
+
+    return (list(edgelist, sizes))
 }
