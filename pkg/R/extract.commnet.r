168dab3f8044ad7bba3865e070f02ac256368247
wlm01 (Dr. ) <wolfgang.mauerer@siemens.com>
2013-10-17 16:36:32 +0000

    Make communication net extraction less R specific
    Also make the output directories slightly systematic.
    
    Signed-off-by: Wolfgang Mauerer <wolfgang.mauerer@siemens.com>
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/snatm@96 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/extract.commnet.r
+++ pkg/R/extract.commnet.r
@@ -1,20 +1,27 @@
-extract.commnet <- 
-function (forest, terms, apply.to = c("subjects", "content"), 
-    list = c("rhelp", "rdevel")) 
-{
+extract.commnet <- function (forest, terms, apply.to, data.path) {
+    if (!(apply.to %in% c("subject", "content")))
+      stop("apply.to must be either 'subject' or 'content'")
+
+    ## NOTE: We assume that the directory has been created by the caller.
+    dir <- file.path(data.path, "commnet.terms", apply.to)
+
     for (i in seq_along(terms)) {
-        if (apply.to == "subjects") {
+        if (apply.to == "subject") {
             net <- createedges(forest, subjectfilter = terms[i])
         }
         if (apply.to == "content") {
             net <- createedges(forest, contentfilter = terms[i])
         }
+        if (is.na(net) || is.null(net))
+          next
+
         if (dim(net)[1] > 0) {
             if (length(grep(terms[i], pattern = "\001|\002|\003|\004|\005|\030|\bb|\037|\a|\021")) == 
                 0) {
                 net <- adjacency(net)
-                save(net, file = file.path(list, apply.to, paste("net_", 
-                  terms[i], ".rda", sep = "")))
+
+                save(net, file = file.path(data.path, "commnet.terms",
+                            apply.to, paste("net_", terms[i], ".rda", sep = "")))
             }
         }
     }
